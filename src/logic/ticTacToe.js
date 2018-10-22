a0079699809824eff13af7f1a36067692dfcfd5a
Hallgrimur Andresson <hallgrimura17@ru.is>
2018-10-22 13:47:36 +0000

    added function xInput and implemented it to return the argument as int

--- src/logic/ticTacToe.js
+++ src/logic/ticTacToe.js
@@ -1,8 +1,20 @@
-// ticTacToe.js
+// src/logic/ticTacToe.js
 class ticTacToe
 {
-    initBoard(){
+    initBoard() {
         return [null, null, null, null, null, null, null, null, null];
+    }
+    game() {
+        var whilePlaying = true;
+        while(whilePlaying) {
+            return "X wins";
+        }
+    }
+    xInput(answer){
+        return parseInt(answer);
+    }
+    oInput(answer){
+        return parseInt(answer);
     }
 }
 
--- src/logic/ticTacToe.test.js
+++ src/logic/ticTacToe.test.js
@@ -1,7 +1,16 @@
-// ticTacToe.test.js
+// src/logic/ticTacToe.test.js
 const ticTacToe = require('./ticTacToe');
 const tester = new ticTacToe();
 
 test("should return an empty array", () => {
     expect(tester.initBoard()).toEqual([null, null, null, null, null, null, null, null, null]);
+});
+test("should return that X won the game", () => {
+    expect(tester.game()).toBe("X wins");
+});
+test("should return field that X chose as input", () => {
+    expect(tester.xInput(5)).toBe(5);
+});
+test("should return field that O chose as input", () => {
+    expect(tester.oInput(1)).toBe(1);
 });
