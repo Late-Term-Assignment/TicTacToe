810b766c3d0d0b672fd329fb1496703e02a5b8e2
dadisteinn <dadisteinn@gmail.com>
2018-10-23 10:47:31 +0000

    should return board array after first move

--- src/logic/ticTacToe.js
+++ src/logic/ticTacToe.js
@@ -1,8 +1,16 @@
 // ticTacToe.js
 class ticTacToe
 {
+  
   constructor(){
       this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      this.playerTurn = "X";
+  }
+
+  makeMove(move){
+    console.log("It is X's turn to move: ");
+    this.board[move - 1] = this.playerTurn;
+    return this.board;
   }
 }
 
--- src/logic/ticTacToe.test.js
+++ src/logic/ticTacToe.test.js
@@ -5,3 +5,7 @@ const tester = new ticTacToe();
 test("should return the board array", () => {
     expect(tester.board).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
+
+test("should return the board array with first move", () => {
+  expect(tester.makeMove(5)).toEqual([1, 2, 3, 4, "X", 6, 7, 8, 9]);
+});
