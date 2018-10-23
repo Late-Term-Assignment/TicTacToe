cdeeafb21e9e6166dc1672adbf0c3452d8e7edcb
Tyler Jones <tylereliasj@gmail.com>
2018-10-23 21:58:06 +0000

    Code refactoring, made some console outputs into a function for clarity, ect

--- src/client/consoleUI.js
+++ src/client/consoleUI.js
@@ -14,8 +14,8 @@ class consoleUI {
     }
 
     beginGame() {
-        console.log("Welcome to Tic-Tak-Toe! ");
-        console.log("The rules are simple, if you are not familiar with them, google them.");
+        console.log("\t\t\tWelcome to Tic-Tac-Toe! ");
+        console.log("\tThe rules are simple, if you are not familiar with them, google them.");
         rl.question('If you want to begin the game, types (b)egin! If you want to quit type (q)uit.\n> ',
         (answer) => {
             if(answer == 'b') {
@@ -33,24 +33,20 @@ class consoleUI {
     }
 
     playersPlay() {
-        console.log(' __________________________________________');
-        console.log('                   SCORE');
-        console.log('      X : ' + game.xWins + '       ' 
-        + 'Draws : ' + game.draws + '     '
-        + 'Y : ' + game.oWins);
-        console.log(' __________________________________________');
+        cui.printBoard();
+        cui.printScore();
         rl.question("It's your turn player " + game.playerTurn + ", please select what number in the board you want to fill in\n > ",
         (answer) => {
-            if(answer > 0 && answer < 10) {
-                if(game.validateInput(answer)) {
-                    game.makeMove(answer);
-                    game.checkWinStatus();
-                    if(game.winStatus != 0) {
-                        game.updateScore();
-                        cui.printWinner(game.winStatus);
+            if(answer > 0 && answer < 10 && game.validateInput(answer)) {
+                game.makeMove(answer);
+                game.checkWinStatus();
+                if(game.winStatus != 0) {
+                    game.updateScore();
+                    cui.printWinner(game.winStatus);
 
-                    }
-                    cui.printBoard();
+                }
+                else {
+                    game.changePlayerTurn();
                     cui.playersPlay();
                 }
             }
@@ -62,33 +58,67 @@ class consoleUI {
     }
 
     printWinner(winner) {
-       
+        cui.printBoard();
+        cui.printScore();
+        if(winner === 1) {
+            console.log("Good job player " + game.playerTurn + " you won!")
+        }
+        else if(winner === 2) {
+            console.log("A draw! What are the odds?");
+        }
+        cui.printGameOver();
     }
 
     printGameOver() {
+        rl.question("Well that was fun, shall we go again? (y/n) or RESET the whole session? (r)\n> ", (answer) => {
+            if(answer === 'y') {
+                game.resetBoard();
+                cui.playersPlay();
+            }
+            else if(answer === 'n') {
+                console.log("Thanks for playing! Game over!");
+            }
+            else if(answer === 'r') {
+                console.log("A new gaming session has started, have fun")
+                game.newSession();
+                cui.playersPlay();
+            }
+            else { 
+                console.log("Illegal character input! Try again");
+                cui.printGameOver();
+            }
+        });
+    }
 
+    printScore() {
+        console.log('\t__________________________________________');
+        console.log('\t                  SCORE');
+        console.log('\t  X : ' + game.xWins + '       ' 
+        + '   Draws : ' + game.draws + '     '
+        + '   Y : ' + game.oWins);
+        console.log('\t__________________________________________\n');
     }
 
     printBoard() {
-        console.log(" _______________________");
-        console.log("|       |       |       |");
-        console.log("|   "
+        console.log("\t\t _______________________");
+        console.log("\t\t|       |       |       |");
+        console.log("\t\t|   "
         + game.board[0] + "   |   " 
         + game.board[1] + "   |   " 
         + game.board[2] + "   |" );
-        console.log("|_______|_______|_______|");
-        console.log("|       |       |       |");
-        console.log("|   " 
+        console.log("\t\t|_______|_______|_______|");
+        console.log("\t\t|       |       |       |");
+        console.log("\t\t|   " 
         + game.board[3] + "   |   " 
         + game.board[4] + "   |   " 
         + game.board[5] + "   |" );
-        console.log("|_______|_______|_______|");
-        console.log("|       |       |       |");
-        console.log("|   " 
+        console.log("\t\t|_______|_______|_______|");
+        console.log("\t\t|       |       |       |");
+        console.log("\t\t|   " 
         + game.board[6] + "   |   " 
         + game.board[7] + "   |   " 
         + game.board[8] + "   |" );
-        console.log("|_______|_______|_______|");
+        console.log("\t\t|_______|_______|_______|");
         
     }
 }
