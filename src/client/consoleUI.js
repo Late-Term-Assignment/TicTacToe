afa58d26d54158b607797e04c5ac65ea9e2386e9
Sindri Dan <sindridan@gmail.com>
2018-10-23 17:14:15 +0000

    some console ui progr. w. Tyler

--- src/client/consoleUI.js
+++ src/client/consoleUI.js
@@ -6,26 +6,44 @@ const game = new ticTacToe();
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+  });
 
 class consoleUI {
     constructor() {
         this.startGame = false;
     }
 
-    beginGame() {
-        //while(!this.startGame) {
-            console.log("Welcome to Tic-Tak-Toe! ");
-            console.log("The rules are simple, if you are not familiar with them, google them.");
-            rl.question('If you want to begin the game, types (b)egin! If you want to quit type (q)uit.',
-            (answer) => {
-                if(answer == 'b') {
-                    console.log("got it");
-                    return this.startGame = true; 
-                };
-            });
-       // }
+    beginGameInput(){
+        
+        
+        return input;
     }
+
+    beginGame(input) {
+        var answer = input;
+        console.log("Welcome to Tic-Tac-Toe! ");
+        console.log("The rules are simple, if you are not familiar with them, google them.");
+        rl.question('If you want to begin the game, types (b)egin! If you want to quit type (q)uit.\t',
+        (answer) => {
+            if(answer === 'b') {
+                var startmsg = "very good, lets go!"
+                rl.close();
+                this.startGame = true; 
+                return answer;
+                //cui.printBoard();
+           }
+           else if(answer === 'q') {
+               rl.close();
+               //console.log("Thank you, come again!");
+               //return answer;
+           }
+           else {
+               console.log("No legal input, try again!"); 
+               //return answer;
+               cui.beginGame();
+           } 
+        });
+    };
 
     printBoard() {
         console.log(" _______________________");
@@ -52,3 +70,6 @@ class consoleUI {
 }
 
 const cui = new consoleUI();
+cui.beginGame();
+//cui.printBoard();
+module.exports = consoleUI;
