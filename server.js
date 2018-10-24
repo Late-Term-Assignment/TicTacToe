ff69fb00e8cacf14be6229127513b66e6e34e7cb
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-24 09:35:43 +0000

    Dev server compiled ui now running on port 5000. API running on port 8080 and 3000

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
+const webpackDevServer = require('webpack-dev-server');
+const webpack = require('webpack');
+
+const config = require('./webpack.config.js');
+const options = {
+  contentBase: './dist',
+  hot: true,
+  host: 'localhost'
+};
+
+webpackDevServer.addDevServerEntrypoints(config, options);
+const compiler = webpack(config);
+const server = new webpackDevServer(compiler, options);
+
+server.listen(5000, 'localhost', () => {
+  console.log('dev server listening on port 5000');
+});
+
 const app = require('./index');
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
 });
+
+/*
+const app = require('./index');
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});*/
