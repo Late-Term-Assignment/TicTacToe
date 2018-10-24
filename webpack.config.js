b6713b2c25dc3ed7f626cf957b04c67cdb62c3a8
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-24 09:37:11 +0000

    Added hotmodules to dev server. Should now auto reload on changes in code

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
     entry: './src/client/index.js',
     output: {
@@ -10,9 +12,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Greeting page'
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
+        contentBase: './dist',
+        hot: true,
         port: 3000,
         open: true,
         compress: true/*
