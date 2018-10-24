96d5a256deb89af7d404db0bdcbfe19b3219a722
Sindri Dan <sindridan@gmail.com>
2018-10-24 22:23:22 +0000

    currentplayerstatus supertest

--- src/server/api.js
+++ src/server/api.js
@@ -3,8 +3,12 @@ const app = express();
 const ticTacToeGame = require("../logic/ticTacToe");
 const game = new ticTacToeGame();
 
-app.get("/ticTacToe", (req, res) => {
+app.get("/ticTacToe/gameBoardStatus", (req, res) => {
 	res.send({"gameBoardStatus": game.board})
+});
+
+app.get("/ticTacToe/currentPlayerStatus", (req, res) => {
+	res.send({"currentPlayerStatus": game.playerTurn})
 });
 
 module.exports = app;
--- src/server/api.test.js
+++ src/server/api.test.js
@@ -2,13 +2,24 @@
 const request = require("supertest")
 const api = require("./api")
 
-describe("GET /api/", () => {
+describe("GET /api/gameBoardStatus", () => {
 	it("should return a 200 OK status code", async () => {
-		const res = await request(api).get("/ticTacToe/");
+		const res = await request(api).get("/ticTacToe/gameBoardStatus");
 		expect(res.status).toBe(200);
 	});
 	it("should return a board initialized with nulls", async () => {
-		const res = await request(api).get("/ticTacToe/");
+		const res = await request(api).get("/ticTacToe/gameBoardStatus");
 		expect(res.body).toEqual({"gameBoardStatus": [1, 2, 3, 4, 5, 6, 7, 8, 9]})
+	});
+});
+
+describe("GET /api/currentPlayerStatus", () => {
+	it("should return a 200 OK status code", async () => {
+		const res = await request(api).get("/ticTacToe/currentPlayerStatus");
+		expect(res.status).toBe(200);
+	});
+	it("should return current player with X", async () => {
+		const res = await request(api).get("/ticTacToe/currentPlayerStatus");
+		expect(res.body).toEqual({"currentPlayerStatus": "X"})
 	});
 });
