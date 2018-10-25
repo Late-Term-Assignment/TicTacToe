abbdde205804504435bc176fe62c333fc95d3a9c
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-25 13:00:36 +0000

    Fixed incorrect path from dist to __dirname

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // index.js
-const app = require("./src/server/api");
+const express = require('express');
+const path = require('path');
+// app = require("./src/server/api");
+var app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+	res.sendFile(__dirname);
+})
 
 const server = require('./server.js');
 
