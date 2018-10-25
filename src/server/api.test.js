d56223156f71ba5e5c3d840cb3fd72ee903d8f92
Sindri Dan <sindridan@gmail.com>
2018-10-25 02:37:34 +0000

    api test for table insert for player O

--- src/server/api.test.js
+++ src/server/api.test.js
@@ -19,10 +19,19 @@ describe("GET /api/currentPlayerStatus", () => {
 });
 
 describe("GET /api/currentPlayerStatusChanged", () => {
-	it("should return current player with X, returning to default", async () => {
+	it("should return current player with O, returning to default", async () => {
 		const api1 = require("./api")
 		const res = await request(api1).get("/ticTacToe/currentPlayerStatusChanged");
 		expect(res.status).toBe(200);
 		expect(res.body).toEqual({"currentPlayerStatusChanged": "O"})
+	});
+});
+
+describe("GET /api/moveMade", () => {
+	it("should return board with index 3 marked as O", async () => {
+		const api2 = require("./api")
+		const res = await request(api2).get("/ticTacToe/moveMade");
+		expect(res.status).toBe(200);
+		expect(res.body).toEqual({"moveMade": [1, 2, "O", 4, 5, 6, 7, 8, 9]})
 	});
 });
