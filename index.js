07b8284b0c2911b45033e8a938429b9856e21936
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-25 18:00:21 +0000

    Api was redundant and removed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // index.js
 const express = require('express');
 const path = require('path');
-// app = require("./src/server/api");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,8 +9,6 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname, index.html);
 });
-
-const server = require('./server.js');
 
 app.listen(PORT, () => {
 	console.log("Server running on port " + PORT);
