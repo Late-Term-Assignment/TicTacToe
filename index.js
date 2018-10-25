a5a0fe2964f73a940c05ef652358cdecd4108655
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-25 11:28:30 +0000

    Testing new server config with heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const app = require("./src/server/api");
 const PORT = process.env.PORT || 3000;
 
+const server = require('./server.js');
+
 app.listen(PORT, () => {
 	console.log("Server running on port " + PORT);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/client/index.js',
+    entry: {
+        app: './src/client/index.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3000,
+        open: true,
+        contentBase: './dist'
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Greeting page'
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
