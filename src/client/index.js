902f71217d6c30a50ed742eb6e314772d6cb0c98
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-10-25 20:01:08 +0000

    Setting everything up for the logic in the WebUI

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,5 @@
 // index.js
 import './styles.css';
-
 const _ = require("lodash");
-
-console.log(__dirname)
-
-function component() {
-    let element = document.createElement('div');
-    return element;
-   }
-document.body.appendChild(component());
+const ticTacToe = require("../logic/ticTacToe")
+game = new ticTacToe();
