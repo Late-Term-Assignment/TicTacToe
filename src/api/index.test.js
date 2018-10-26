403f0d648997b6d7271665f4751da2e0f846f6d6
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-26 17:15:47 +0000

    Code cleanup in the api tests

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,17 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
 
-// describe('POST /newGame', function() {
-//     it('should return 200 status code', function(done) {
-//         request(app)
-//             .post('/newGame')
-//             .expect(200)
-//             .end(function(err, res) {
-//             if (err) return done(err);
-//             done();
-//         });
-//     });
-// });
 
 
 describe("GET /newGame", () => {
@@ -20,13 +9,6 @@ describe("GET /newGame", () => {
         expect(res.status).toBe(200);
     });
 });
-
-// describe("GET /getBoard", () => {
-//     it("should check if /getBoard is up and running", async () => {
-//         const res = await request(app).get("http://localhost:3000/getBoard");
-//         expect(res.status).toBe(200);
-//     });
-// });
 
 describe("POST /makeMove/5", () => {
     it("should be able to make a move", async () => {
@@ -38,12 +20,3 @@ describe("POST /makeMove/5", () => {
         expect(resss.body).toEqual({ "GameBoard": ["X", "O", 3, 4, 5, 6, "O", 8, "X"]});
     });
 })
-
-// describe("POST /makeMove/{move}", () => {
-//     it("should make a single move and return the new game board", async () => {
-//         const status = await request(app).post("/makeMove/5");
-//         const res = await request(app).get("/getBoard");
-//         expect(status.status).toBe(200);
-//         expect(res.body).toBe({ "GameBoard": ["1", "2", "3", "4", "X", "6", "7", "8", "9"] });
-//     });
-// });
