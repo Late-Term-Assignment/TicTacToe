1df11a3c36d6239c7c0f2762f3e79bafa26cd275
Hallgrimur Andresson <hallgrimura17@ru.is>
2018-10-26 20:26:01 +0000

    removed webLogic.test.js since it cant be used to test webLogic.js, implemented initialize, makeMove and fetchThis partially to get shallow functionality

--- src/logic/WebLogic.js
+++ src/logic/WebLogic.js
@@ -1,30 +1,40 @@
 // webLogic.js
-const ticTacToe = require('./ticTacToe');
-class webLogic
-{
-    constructor(){
-        this.game = new ticTacToe();
+function initialize(){
+    for(var i = 0; i < 9; i++){
+        document.getElementById(i.toString()).addEventListener("click", makeMove);
     }
 }
-module.exports = webLogic;
-
-
-
-/*fetch("https://arcane-crag-40980.herokuapp.com/")
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-      });
+initialize();
+function makeMove()
+{
+    function enableTxt(elem) {
+      var id = $(elem).attr("id");
+      fetchThis("makeMove/" + id);
+      this.removeEventListener("click", makeMove);
+      //fetchThis("getBoard");
+      this.innerHTML = "Y";
     }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });*/
+}
+function fetchThis(toFetch)
+{
+  fetch("http://localhost:3000/" + toFetch)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+  
+        // Examine the text in the response
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+}
+
+
