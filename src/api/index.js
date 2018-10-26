5bcd3828d04294128de18f48a9761584eac4fd00
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-10-26 14:42:17 +0000

    The api is working, but needs work to fully work

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const router = express.Router();
-//const ticTacToe = require("../logic/ticTacToe");
-//const game = new ticTacToe();
 
-router.post("/newGame", (req, res) => {
-    game.resetBoard();
-    res.send('Ok');
+const ticTacToe = require('../logic/ticTacToe')
+const serverSideGame = new ticTacToe();
+
+
+router.get("/newGame", (req, res) => {
+    serverSideGame.resetBoard();
+    res.status(200).send({"game": serverSideGame});
 });
   
 router.post("/getBoard", (req, res) => {
-    res.send({ "GameBoard": req.body.game.board });
+    res.send({ "GameBoard": serverSideGame.board });
 });
 
 router.post("/makeMove/:move", (req, res) => {
-    game.makeMove(req.move);
-    res.send('Ok');
+    serverSideGame.makeMove(req.params.move);
+    serverSideGame.changePlayerTurn();
+    res.status(200).send('Ok');
 });
 
 module.exports = router;
--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,50 +1,49 @@
 const request = require("supertest");
 const app = require("../app");
 
-const ticTacToe = require("../logic/ticTacToe");
-const game = new ticTacToe();
-const fetch = require('node-fetch');
+// describe('POST /newGame', function() {
+//     it('should return 200 status code', function(done) {
+//         request(app)
+//             .post('/newGame')
+//             .expect(200)
+//             .end(function(err, res) {
+//             if (err) return done(err);
+//             done();
+//         });
+//     });
+// });
 
-/*
-describe('POST /newGame', function() {
-    it('should return 200 status code', function(done) {
-        request(app)
-            .post('/newGame')
-            .expect(200)
-            .end(function(err, res) {
-            if (err) return done(err);
-            done();
-        });
+
+describe("GET /newGame", () => {
+    it("should return the game board in its initial state", async () => {
+        const res = await request(app).get("/newGame");
+        expect(res.status).toBe(200);
     });
 });
 
-describe('GET /getBoard', function() {
-    it('should return the current game board', function(done) {
-        request(app)
-            .get('/getBoard')
-            .expect(200)
-            .expect({"GameBoard": [1, 2, 3, 4, 5, 6, 7, 8, 9]})
-            .end(function(err, res) {
-            if (err) return done(err);
-            done();
-        });
-    });
-});
-*/
+// describe("GET /getBoard", () => {
+//     it("should check if /getBoard is up and running", async () => {
+//         const res = await request(app).get("http://localhost:3000/getBoard");
+//         expect(res.status).toBe(200);
+//     });
+// });
 
-describe("GET /getBoard", () => {
-    it("should return the current game board", async () => {
-        fetch('localhost:3000/getBoard', {method: 'POST', body: game})
-        .then( res => expect(res.sjon()).toBe({ "GameBoard": [1, 2, 3, 4, 5, 6, 7, 8, 9] }));
+describe("POST /makeMove/5", () => {
+    it("should be able to make a move", async () => {
+        const one = await request(app).post("/makeMove/9")
+        const two= await request(app).post("/makeMove/7")
+        const three= await request(app).post("/makeMove/1")
+        const four= await request(app).post("/makeMove/2")
+        const resss = await request(app).post("/getBoard")
+        expect(resss.body).toEqual({ "GameBoard": ["X", "O", 3, 4, 5, 6, "O", 8, "X"]});
     });
-});
+})
 
-/*
-describe("POST /makeMove/{move}", () => {
-    it("should make a single move and return the new game board", async () => {
-        const status = await request(app).post("/makeMove/5");
-        const res = await request(app).get("/getBoard");
-        expect(status.status).toBe(200);
-        expect(res.body).toBe({ "GameBoard": ["1", "2", "3", "4", "X", "6", "7", "8", "9"] });
-    });
-});*/
+// describe("POST /makeMove/{move}", () => {
+//     it("should make a single move and return the new game board", async () => {
+//         const status = await request(app).post("/makeMove/5");
+//         const res = await request(app).get("/getBoard");
+//         expect(status.status).toBe(200);
+//         expect(res.body).toBe({ "GameBoard": ["1", "2", "3", "4", "X", "6", "7", "8", "9"] });
+//     });
+// });
