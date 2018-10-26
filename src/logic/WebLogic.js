45b2a063e57f1a116d5a2c4bfc75d6c4a9f2860c
Hallgrimur Andresson <hallgrimura17@ru.is>
2018-10-26 18:02:30 +0000

    implemented class webLogic that initializes an instance of ticTacToe

--- src/logic/WebLogic.js
+++ src/logic/WebLogic.js
@@ -1,1 +1,30 @@
 // webLogic.js
+const ticTacToe = require('./ticTacToe');
+class webLogic
+{
+    constructor(){
+        this.game = new ticTacToe();
+    }
+}
+module.exports = webLogic;
+
+
+
+/*fetch("https://arcane-crag-40980.herokuapp.com/")
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });*/
--- src/logic/webLogic.test.js
+++ src/logic/webLogic.test.js
@@ -1,1 +1,7 @@
 // webLogic.test.js
+const webLogic = require('./webLogic');
+const tester = new webLogic();
+test("should return and instance of tictactoe", () => {  
+    expect(tester).toEqual({"game": {"board": [1, 2, 3, 4, 5, 6, 7, 8, 9], "draws": 0, "nrOfmoves": 9, "oWins": 0, "playerTurn": "X", "winStatus": 0, "xWins": 0}});
+  });
+  
