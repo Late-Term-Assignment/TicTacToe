f07ad3af8e577680eecdc05d87c5f40151022e3a
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-26 17:31:14 +0000

    Added a test that tests multiple inserts

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../app");
 
+// Get new game board
 describe("GET /newGame", () => {
     it("should return the game board in its initial state", async () => {
         const res = await request(app).get("/newGame");
@@ -9,10 +10,25 @@ describe("GET /newGame", () => {
     });
 });
 
+
+// Make a single move
 describe("POST /makeMove/5", () => {
     it("should be able to make a move", async () => {
+        await request(app).get("/newGame");
         const one = await request(app).post("/makeMove/5")
         const res = await request(app).post("/getBoard")
         expect(res.body).toEqual({ "GameBoard": [1, 2, 3, 4, "X", 6, 7, 8, 9]});
     });
-})
+});
+
+// Make multiple moves
+describe("POST /makeMove/5, /makeMove/7, /makeMove/9", () => {
+    it("should be able to make a move", async () => {
+        await request(app).get("/newGame");
+        const one = await request(app).post("/makeMove/5")
+        const two = await request(app).post("/makeMove/7")
+        const three = await request(app).post("/makeMove/9")
+        const res = await request(app).post("/getBoard")
+        expect(res.body).toEqual({ "GameBoard": [1, 2, 3, 4, "X", 6, "O", 8, "X"]});
+    });
+});
