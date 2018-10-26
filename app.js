5bd6c4ec0a9349084afe4dad81d2cdd86114ecf2
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-26 10:44:41 +0000

    Removed old code

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-// app.js
-//const express = require('express');
-//const path = require('path');
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.set("port", PORT);
 app.listen(PORT);
 console.log("Server running on port " + PORT);
-
-/*
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-	res.sendFile(__dirname);
-});
-app.listen(PORT, () => {
-	console.log("Server running on port " + PORT);
-});
-*/
-
-
-//module.exports = app;
