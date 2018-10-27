105d701a4a3ef370a62153e69b080cda21b958b1
Hallgrimur Andresson <hallgrimura17@ru.is>
2018-10-27 15:48:22 +0000

    now clears all in the field when new game is pressed

--- src/logic/WebLogic.js
+++ src/logic/WebLogic.js
@@ -1,5 +1,5 @@
 // webLogic.js
-initialize();
+newGame();
 function initialize(){
     for(var i = 0; i < 9; i++){
         document.getElementById(i.toString()).addEventListener("click", makeMove);
@@ -9,33 +9,44 @@ function initialize(){
 
 }
 function newGame(){
-    fetchGet("newGame");
+    var board = fetchGet("/newGame");
+    console.log(board);
+    document.getElementById("score1").innerHTML = 0;
+    document.getElementById("score2").innerHTML = 0;
+    document.getElementById("draws").innerHTML = 0;
+    initialize();
+    for(var i = 0; i < 9; i++)
+    {
+        document.getElementById(i.toString()).innerHTML = null;
+    }
 }
 function reset(){
+    fetchGet("/resetGame");
 }
 function makeMove(){
-    fetchPost("makeMove/" + this.id);
+    //var board = fetchGet("/makeMove/" + this.id);
+    //console.log(board);
     this.removeEventListener("click", makeMove);
     this.innerHTML = "Y";
 }
-function fetchPost(toFetch){
-  fetch("/" + toFetch, {method: 'POST'})
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
+function fetchGet(toFetch){
+    fetch(toFetch, {method: 'GET'})
+      .then(
+        function(response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+    
+          // Examine the text in the response
+          response.json().then(function(data) {
+              console.log(data);
+            return data;
+          });
         }
-  
-        // Examine the text in the response
-        response.json().then(function(data) {
-          console.log(data);
-          return data;
-        });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-}
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
