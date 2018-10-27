f3e3deab3b8a0fc7929b109b54139f56c8ea1390
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-27 12:24:20 +0000

    Changed the game status from 0-2 to 0-3, x,o,draw,in progress status

--- src/logic/ticTacToe.js
+++ src/logic/ticTacToe.js
@@ -5,7 +5,7 @@ class ticTacToe
 //      0 - Game in progress/not started
 //      1 - X has won
 //      2 - Y has won
-//      3 - tie
+//      3 - draw
 
   constructor(){
       this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -22,9 +22,9 @@ class ticTacToe
     if(this.validateInput(move)) {
       this.board[move - 1] = this.playerTurn;
       this.nrOfmoves++;
-      this.changePlayerTurn();
       this.checkWinStatus();
       this.updateScore();
+      this.changePlayerTurn();
     }
   }
 
@@ -46,34 +46,44 @@ class ticTacToe
   checkWinStatus(){
     for(var i = 0; i < 9; i += 3){
       if(this.board[i] === this.board[i+1] && this.board[i] === this.board[i+2]){
-        this.winStatus = 1;
+        if(this.playerTurn === "X") {
+          this.winStatus = 1;
+        } else {
+          this.winStatus = 2;
+        }
         return;
       }
     }
     for(var i = 0; i < 3; i++){
       if(this.board[i] === this.board[i+3] && this.board[i] === this.board[i+6]){
-        this.winStatus = 1;
+        if(this.playerTurn === "X") {
+          this.winStatus = 1;
+        } else {
+          this.winStatus = 2;
+        }
         return;
       }
     }
     if(this.board[0] === this.board[4] && this.board[0] === this.board[8] || 
        this.board[2] === this.board[4] && this.board[2] === this.board[6]){
-      this.winStatus = 1;
+        if(this.playerTurn === "X") {
+          this.winStatus = 1;
+        } else {
+          this.winStatus = 2;
+        }
       return;
     }
     
     if(this.nrOfmoves === 9 && !this.winStatus){
-      this.winStatus = 2;
+      this.winStatus = 3;
     }
   }
 
   updateScore(){
     if(this.winStatus === 1){
-      if(this.playerTurn === "O"){
         this.xWins++;
-      } else{
+      } else if(this.winStatus === 2) {
         this.oWins++;
-      }
     }
     else{
       this.draws++;
