44c8c3260be772dca753a1a8c0a16cfcdd61cd9d
Tyler Jones <tylereliasj@gmail.com>
2018-10-27 15:18:50 +0000

    Added the configuration

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var options =  [
+    verbose = true,
+    dry = false,
+    watch = true
+];
+
+var paths = [
+    'dist'
+];
+
+module.exports = {
+    entry: {
+        app: './src/app.js';
+    },
+    plugins: [
+        new CleanWebPackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
+    output: {
+        filename: '[name].main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    }
+};
