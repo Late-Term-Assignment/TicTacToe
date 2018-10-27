a07774e2289e6fc859deefec781e0d4fb1195a34
Tyler Jones <tylereliasj@gmail.com>
2018-10-27 17:03:28 +0000

    fixed the webpack to work

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,8 +3,4 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production'
-    // node: {
-    //     fs: 'empty',
-    //     net: 'empty'
-    //   }
 });
