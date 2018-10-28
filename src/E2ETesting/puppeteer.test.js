20b8e29ab5e79b2c7ce87da8eb7efee30d19850c
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-10-28 18:21:01 +0000

    Added a test so that X wins

--- src/E2ETesting/puppeteer.test.js
+++ src/E2ETesting/puppeteer.test.js
@@ -1,40 +1,60 @@
 const puppeteer = require('puppeteer');
-describe("X input Pupp Test", () => { 
+describe("X input Pupp Test", () => {
     beforeAll(async () => {
         browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             slowMo: 80
+        });
+        page = await browser.newPage();
+        await page.setViewport({
+            width: 1280,
+            height: 800
         });
     });
 
+
     test("Player X can input in top left square", async () => {
-            jest.setTimeout(12000);
-            const page = await browser.newPage();
-            await page.setViewport({ width: 1280, height: 800 })
-            await page.goto('https://tictactoe1inarow.herokuapp.com/')  
-          
-            //await for the selection, then press square 1 with player x.
-            await page.waitForSelector("#s1") 
-            await page.click("#s1")
-            var tmp = await page.$eval("#s1", e => e.textContent);
-            expect(tmp).toBe("X");
+        jest.setTimeout(12000);
+
+        await page.goto('https://tictactoe1inarow.herokuapp.com/')
+
+        //await for the selection, then press square 1 with player x.
+        await page.waitForSelector("#s1")
+        await page.click("#s1")
+        var tmp = await page.$eval("#s1", e => e.textContent);
+        expect(tmp).toBe("X");
     });
     test("Player O can input in top left square", async () => {
         jest.setTimeout(12000);
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1280, height: 800 })
-        await page.goto('https://tictactoe1inarow.herokuapp.com/')  
-      
-        //await for the selection, then press square 1 with player x.
-        await page.waitForSelector("#s1") 
+        await page.goto('https://tictactoe1inarow.herokuapp.com/')
+
+        // Start by clicking the upper-left corner and then click the top-middle square
+        await page.waitForSelector("#s1")
         await page.click("#s1")
-        await page.waitForSelector("#s2") 
+        await page.waitForSelector("#s2")
         await page.click("#s2")
         var tmp = await page.$eval("#s2", e => e.textContent);
         expect(tmp).toBe("O");
-});
+    });
+
+    test("Player X wins the game and check if the correct text is displayed", async () => {
+        jest.setTimeout(12000);
+        await page.goto('https://tictactoe1inarow.herokuapp.com/')
+        // Tests that X wins by selecting the correct squares correctly
+        await page.waitForSelector("#s1")
+        await page.click("#s1")
+        await page.waitForSelector("#s2")
+        await page.click("#s2")
+        await page.waitForSelector("#s4")
+        await page.click("#s4")
+        await page.waitForSelector("#s5")
+        await page.click("#s5")
+        await page.waitForSelector("#s7")
+        await page.click("#s7")
+        var tmp = await page.$eval("#message", e => e.textContent);
+        expect(tmp).toBe("X wins");
+    });
     afterAll(() => {
         browser.close();
     });
 });
-
