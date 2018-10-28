b467b3ed6872214d5a2d9389d9d11f1c5681e8ca
Tyler Jones <tylereliasj@gmail.com>
2018-10-28 12:36:47 +0000

    Code refactoring

--- markdown-pfd.js
+++ markdown-pfd.js
@@ -1,15 +1,7 @@
-
-
-
 var markdownpdf = require("markdown-pdf")
   , fs = require("fs")
  
-// fs.createReadStream("./docs/AdministrationManual.md")
-//   .pipe(markdownpdf())
-//   .pipe(fs.createWriteStream("./docs/pdf/AdministrationManual.pdf"));
- 
-// --- OR ---
- 
+
 markdownpdf().from("./docs/AdministrationManual.md").to("./docs/pdf/AdministrationManual.pdf", function () {
   console.log("Done")
 });
