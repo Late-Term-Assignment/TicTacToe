83f6453649b77e123b1bcd0be9583fe6b79ef727
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-10-28 18:12:32 +0000

    Added a end-to-end test for when O makes a move

--- src/E2ETesting/puppeteer.test.js
+++ src/E2ETesting/puppeteer.test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 describe("X input Pupp Test", () => { 
     beforeAll(async () => {
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             slowMo: 80
         });
     });
@@ -19,6 +19,20 @@ describe("X input Pupp Test", () => {
             var tmp = await page.$eval("#s1", e => e.textContent);
             expect(tmp).toBe("X");
     });
+    test("Player O can input in top left square", async () => {
+        jest.setTimeout(12000);
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 800 })
+        await page.goto('https://tictactoe1inarow.herokuapp.com/')  
+      
+        //await for the selection, then press square 1 with player x.
+        await page.waitForSelector("#s1") 
+        await page.click("#s1")
+        await page.waitForSelector("#s2") 
+        await page.click("#s2")
+        var tmp = await page.$eval("#s2", e => e.textContent);
+        expect(tmp).toBe("O");
+});
     afterAll(() => {
         browser.close();
     });
