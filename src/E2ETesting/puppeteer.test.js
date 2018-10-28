dd9466b3c76109fa8dd26cf9369dc993959d4dca
Sindri Dan <sindridan@gmail.com>
2018-10-28 20:21:46 +0000

    horizontal win test for O

--- src/E2ETesting/puppeteer.test.js
+++ src/E2ETesting/puppeteer.test.js
@@ -64,10 +64,10 @@ describe("Puppeteer testing for game", () => {
         expect(square).toBe("O");
     });
 
-    test("Player X wins the game and check if the correct text on the site is displayed", async () => {
+    test("Player X wins the game, vertical, and check if the correct text on the site is displayed", async () => {
         jest.setTimeout(12000);
         await page.goto('https://tictactoe1inarow.herokuapp.com/')
-        // Tests that X wins by selecting the correct squares correctly
+        // Tests that X wins, vertically, by selecting the correct squares correctly
         await page.waitForSelector("#reset")
         await page.click("#reset")
         await page.waitForSelector("#s1")
@@ -90,10 +90,10 @@ describe("Puppeteer testing for game", () => {
         expect(draws).toBe("0");
     });
 
-    test("Player O wins the game and check if the correct text is displayed", async () => {
+    test("Player O wins the game, vertical, and check if the correct text is displayed", async () => {
         jest.setTimeout(12000);
         await page.goto('https://tictactoe1inarow.herokuapp.com/')
-        // Test that O wins and if the score updates correctly
+        // Test that O wins, vertically, and if the score updates correctly
         await page.waitForSelector("#reset")
         await page.click("#reset")
         await page.waitForSelector("#s1")
@@ -121,7 +121,7 @@ describe("Puppeteer testing for game", () => {
     test("Player X wins the game, diagonally, and check if the correct text is displayed", async () => {
         jest.setTimeout(12000);
         await page.goto('https://tictactoe1inarow.herokuapp.com/')
-        // Test that O wins and if the score updates correctly
+        // Test that X wins, diagonally, and if the score updates correctly
         await page.waitForSelector("#reset")
         await page.click("#reset")
         await page.waitForSelector("#s1")
@@ -142,6 +142,34 @@ describe("Puppeteer testing for game", () => {
         expect(message).toBe("X wins");
         expect(player1).toBe("1");
         expect(player2).toBe("0");
+        expect(draws).toBe("0");
+    });
+
+    test("Player O wins the game, horizontally, and check if the correct text is displayed", async () => {
+        jest.setTimeout(12000);
+        await page.goto('https://tictactoe1inarow.herokuapp.com/')
+        // Test that O wins, horizontally, and if the score updates correctly
+        await page.waitForSelector("#reset")
+        await page.click("#reset")
+        await page.waitForSelector("#s1")
+        await page.click("#s1")
+        await page.waitForSelector("#s4")
+        await page.click("#s4")
+        await page.waitForSelector("#s2")
+        await page.click("#s2")
+        await page.waitForSelector("#s5")
+        await page.click("#s5")
+        await page.waitForSelector("#s9")
+        await page.click("#s9")
+        await page.waitForSelector("#s6")
+        await page.click("#s6")
+        var message = await page.$eval("#message", e => e.textContent);
+        var player1 = await page.$eval("#score1", e => e.textContent);
+        var player2 = await page.$eval("#score2", e => e.textContent);
+        var draws = await page.$eval("#draws", e => e.textContent);
+        expect(message).toBe("O wins");
+        expect(player1).toBe("0");
+        expect(player2).toBe("1");
         expect(draws).toBe("0");
     });
 
