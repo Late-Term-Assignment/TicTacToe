aa36d7ea9fdbc514a758802813e8ca0c4968eba1
Brynjar Grétarsson <brynjarorng@gmail.com>
2018-10-28 15:38:22 +0000

    Re added the dev server and dev client. Need to run both for active building and refreshing

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,22 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
     mode: 'development',
     watch:true,
     devtool: 'inline-source-map',
-    watchOptions: {
-        poll: true
-      }
+    devServer: {
+        port: 6060,
+        open: true,
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        watchOptions: {
+            poll: true
+        },
+        watchContentBase: true,
+        proxy: {
+            "/": "http://localhost:3000"
+        }
+    }
 });
